f8a9a0dd50909fdc372f2ec4729992591ae0dd67
dachibox <dachi.gogotchuri@gmail.com>
2015-12-01 10:19:06 +0100

    Implemented custom dialog

--- Geeky.UI.Samples/MainPage.xaml.cs
+++ Geeky.UI.Samples/MainPage.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -12,6 +15,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Geeky.UI.Annotations;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -20,16 +24,70 @@ namespace Geeky.UI.Samples
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
     {
         public MainPage()
         {
             this.InitializeComponent();
+
+            Items = new ObservableCollection<string>();
+            for (int i = 0; i < 19; i++)
+            {
+                Items.Add(string.Format(
+                    "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta)  {0}", i));
+            }
         }
 
-        private async void ShowDialog_Click(object sender, RoutedEventArgs e)
+        private bool isOpen;
+        public bool IsOpen
         {
-            var result = await Dialog.ShowAsync();
+            get { return isOpen; }
+            set
+            {
+                if (isOpen != value)
+                {
+                    isOpen = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+        private void ShowDialog_Click(object sender, RoutedEventArgs e)
+        {
+            IsOpen = true;
+        }
+
+        private ObservableCollection<string> items;
+        public ObservableCollection<string> Items
+        {
+            get { return items; }
+            set
+            {
+                if (items == value) return;
+                items = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string item;
+
+        public string Item
+        {
+            get { return item; }
+            set
+            {
+                if (item == value) return;
+                item = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
---
+++ Geeky.UI/Dialog.cs
@@ -0,0 +1,145 @@
+﻿using System.Windows.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+
+// The Templated Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234235
+
+namespace Geeky.UI
+{
+    [TemplatePart(Name = PART_ROOT_BORDER, Type = typeof(Border))]
+    [TemplatePart(Name = PART_ROOT_GRID, Type = typeof(Grid))]
+    [TemplatePart(Name = PART_BACK_BUTTON, Type = typeof(Button))]
+    [TemplatePart(Name = PART_CONTENT, Type = typeof(ContentPresenter))]
+    [TemplatePart(Name = PART_DIALOGGRID, Type = typeof(Grid))]
+    public sealed class Dialog : ContentControl
+    {
+        private const string PART_ROOT_BORDER = "PART_RootBorder";
+        private const string PART_ROOT_GRID = "PART_RootGrid";
+        private const string PART_BACK_BUTTON = "PART_BackButton";
+        private const string PART_CONTENT = "PART_Content";
+        private const string PART_DIALOGGRID = "PART_DialogGrid";
+
+        private Grid rootGrid;
+        private Border rootBorder;
+        private Button backButton;
+        private Grid dialogGrid;
+        private ContentPresenter contentPresenter;
+
+        public event RoutedEventHandler BackButtonClicked;
+
+        public Dialog()
+        {
+            this.DefaultStyleKey = typeof(Dialog);
+        }
+
+        protected override void OnApplyTemplate()
+        {
+            rootBorder = (Border) GetTemplateChild(PART_ROOT_BORDER);
+            rootGrid = (Grid) GetTemplateChild(PART_ROOT_GRID);
+            backButton = (Button) GetTemplateChild(PART_BACK_BUTTON);
+            dialogGrid = (Grid) GetTemplateChild(PART_DIALOGGRID);
+            contentPresenter = (ContentPresenter) GetTemplateChild(PART_CONTENT);
+
+            ResizeContainers();
+
+            if (backButton != null)
+                backButton.Click += BackButton_Click;
+
+            Window.Current.SizeChanged += OnWindowSizeChanged;
+            Unloaded += OnUnloaded;
+
+            base.OnApplyTemplate();
+        }
+
+        private void ResizeContainers()
+        {
+            if (rootGrid != null)
+            {
+                rootGrid.Width = Window.Current.Bounds.Width;
+                rootGrid.Height = Window.Current.Bounds.Height;
+                dialogGrid.Margin = new Thickness(0,80,0,80);
+                //contentPresenter.Width = Window.Current.Bounds.Width;
+            }
+
+            if (rootBorder != null)
+            {
+                rootBorder.Width = Window.Current.Bounds.Width;
+            }
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (BackButtonClicked != null)
+                BackButtonClicked(sender, e);
+            else
+                IsOpen = false;
+        }
+
+        private void OnWindowSizeChanged(object sender, Windows.UI.Core.WindowSizeChangedEventArgs e)
+        {
+            ResizeContainers();
+        }
+
+        private void OnUnloaded(object sender, RoutedEventArgs e)
+        {
+            Unloaded -= OnUnloaded;
+            Window.Current.SizeChanged -= OnWindowSizeChanged;
+        }
+
+        public static readonly DependencyProperty BackButtonVisibilityProperty = DependencyProperty.Register(
+            "BackButtonVisibility", typeof (Visibility), typeof (Dialog), new PropertyMetadata(Visibility.Collapsed));
+
+        public Visibility BackButtonVisibility
+        {
+            get { return (Visibility) GetValue(BackButtonVisibilityProperty); }
+            set { SetValue(BackButtonVisibilityProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsOpenProperty = DependencyProperty.Register(
+            "IsOpen", typeof (bool), typeof (Dialog), new PropertyMetadata(false, OnIsOpenPropertyChanged));
+
+        public bool IsOpen
+        {
+            get { return (bool) GetValue(IsOpenProperty); }
+            set { SetValue(IsOpenProperty, value); }
+        }
+
+        private static void OnIsOpenPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (!(bool) e.NewValue) return;
+            var dlg = d as Dialog;
+            dlg?.ApplyTemplate();
+        }
+
+
+        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register(
+            "Title", typeof (string), typeof (Dialog), null);
+
+        public string Title
+        {
+            get { return (string) GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+
+        public static readonly DependencyProperty BackButtonCommandProperty = DependencyProperty.Register(
+            "BackButtonCommand", typeof (ICommand), typeof (Dialog), new PropertyMetadata(DependencyProperty.UnsetValue));
+
+        public ICommand BackButtonCommand
+        {
+            get { return (ICommand) GetValue(BackButtonCommandProperty); }
+            set { SetValue(BackButtonCommandProperty, value); }
+        }
+
+
+        public static readonly DependencyProperty BackButtonCommandParameterProperty = DependencyProperty.Register(
+            "BackButtonCommandParameter", typeof (object), typeof (Dialog), new PropertyMetadata(DependencyProperty.UnsetValue));
+
+        public object BackButtonCommandParameter
+        {
+            get { return (object) GetValue(BackButtonCommandParameterProperty); }
+            set { SetValue(BackButtonCommandParameterProperty, value); }
+        }
+    }
+}
